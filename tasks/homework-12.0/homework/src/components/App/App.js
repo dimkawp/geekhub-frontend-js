1a53cd41838ecab2003e12474a7ce3c5a329df73
dimkawp <dimkawp@gmail.com>
2018-02-02 18:39:38 +0200

    work on homework-12.0.  - install React.  - add sass.

---
+++ tasks/homework-12.0/homework/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>TEST</p>
+      </div>
+    );
+  }
+}
+
+export default App;
