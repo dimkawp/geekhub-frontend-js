a6f0ef69aa2fe71f1bd49ce87c40ebb7ad668cd3
dimkawp <dimkawp@gmail.com>
2018-03-07 16:17:27 +0200

    homework-13.0
    complite work on 'total sales' block.

--- tasks/homework-13.0/homework/src/components/Main/Statistics/TotalSales.js
+++ tasks/homework-13.0/homework/src/components/Main/Statistics/TotalSales.js
@@ -1,56 +1,72 @@
 import React, { Component } from 'react';
 import ReactHighcharts from 'react-highcharts';
-import Highcharts from 'highcharts';
-import ReactHighchartsMore from 'highcharts-more';
-import HighchartsSolidGauge from 'highcharts-solid-gauge';
-//config
-import SolidChart from '../../../config/solidGauge.config';
 
 class TotalSales extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      First: SolidChart,
-      FirstTotalfieldVal: [
-        {y: 70}
-      ]
     }
   }
   componentDidMount() {
     const $ = window.$;
-
-
-    function getAttributes ( $node ) {
-      var attrs = {};
-      $.each( $node[0].attributes, function ( index, attribute ) {
-          attrs[attribute.name] = attribute.value;
-      } );
-  
-      return attrs;
-  }
-  getAttributes(".highcharts-pane-group");
-    
-
+    $('.demo-1').percentcircle(
+      {
+        coverBg: '#fff',
+        bgColor: '#efefef',
+        fillColor: '#5484FF'
+      }
+    );
+    $('.demo-2').percentcircle(
+      {
+        coverBg: '#fff',
+        bgColor: '#efefef',
+        fillColor: '#AA5FB9'
+      }
+    );
+    $('.demo-3').percentcircle(
+      {
+        coverBg: '#fff',
+        bgColor: '#efefef',
+        fillColor: '#F83C7B'
+      }
+    );
   }
   
   render() {
     
     console.log("TotalSalesComponents");
-    SolidChart.pane.background[0].backgroundColor = 'red';
 
     return (
         <div className="totalSales">
+          <div className="header">
+            <h3>Total sales</h3>
+            <div className="buttonBlock">
+              <button>1</button>
+              <button>2</button>
+            </div>
+          </div>
           <div className="items">
-            <div className="item col-sm-12 col-lg-3">
-            <ReactHighcharts config={SolidChart}></ReactHighcharts>
+            <div className="item col-sm-12 col-lg-4">
+             <div className="demo-1" data-percent="45"></div>
+             <div className="resultBlock">
+              <p>2,300$</p>
+              <span>Direct Sales</span>
+             </div>
             </div>
-            <div className="item col-sm-12 col-lg-3">
-
+            <div className="item col-sm-12 col-lg-4">
+              <div className="demo-2" data-percent="20"></div>
+              <div className="resultBlock">
+              <p>2,300$</p>
+              <span>Direct Sales</span>
+             </div>
             </div>
-            <div className="item col-sm-12 col-lg-3">
-            
+            <div className="item col-sm-12 col-lg-4">
+              <div className="demo-3" data-percent="25"></div>   
+              <div className="resultBlock">
+              <p>2,300$</p>
+              <span>Direct Sales</span>
+             </div>  
             </div>
-
           </div>
         </div>
     );
