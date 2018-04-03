7214b82feec38ebb82f4bf42653565f74969c0ea
dimkawp <dimkawp@gmail.com>
2018-04-03 16:19:05 +0300

    homework-14.0
    top,side menu.

--- tasks/homework-14.0/homework/src/components/Main/Menu/HeaderMenu.js
+++ tasks/homework-14.0/homework/src/components/Main/Menu/HeaderMenu.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import './HeaderMenu.css';
+import logo from '../../../assets/img/logo.fw.png';
+import IconSearch from 'react-icons/lib/fa/search';
+import IconBell from 'react-icons/lib/fa/bell-o';
+import { Dropdown } from 'semantic-ui-react'
+
 
 class HeaderMenu extends Component {
 
     render() {
         console.log("HeaderMenuComponents");
         return (
-            <div>
-                HeaderMenu
-                <button onClick={this.Logout}>Logout</button>
+            <div className="headerBlock">
+                <div className="logo">
+                    <img src={logo} alt="logo"/>
+                </div>
+                <div className="userBlock">
+                    <button className="buttonAdd" onClick={this.Logout}>Logout</button>
+                    <div className="search"><IconSearch/></div>
+                    <div className="events"><IconBell/></div>
+                    <img src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar"/>
+                    <Dropdown text=''>
+                        <Dropdown.Menu>
+                            <Dropdown.Item text='New' />
+                            <Dropdown.Item text='Open...' description='ctrl + o' />
+                            <Dropdown.Item icon='folder' text='Move to folder' />
+                            <Dropdown.Item icon='trash' text='Move to trash' />
+                            <Dropdown.Divider />
+                            <Dropdown.Item text='Download As...' />
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+
             </div>
         );
     }
--- tasks/homework-14.0/homework/src/components/Main/Menu/SideMenu.js
+++ tasks/homework-14.0/homework/src/components/Main/Menu/SideMenu.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './SideMenu.css';
+import IconHome from 'react-icons/lib/fa/home'
+import IconBars from 'react-icons/lib/fa/bars'
+import IconChart from 'react-icons/lib/fa/line-chart'
+import IconMail from 'react-icons/lib/fa/envelope'
+import IconUser from 'react-icons/lib/fa/user'
 
 class SideMenu extends Component {
 
     render() {
         console.log("SideMenuComponents");
         return (
-            <div>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/workflow">Workflow</Link></li>
-                    <li><Link to="/inbox">Inbox</Link></li>
-                    <li><Link to="/projects">Projects</Link></li>
-                    <li><Link to="/raport">Raport</Link></li>
-                </ul>
+            <div className="sideMenuBlock">
+                <div className="items">
+                    <div className="item"><NavLink to="/" activeClassName="active"><IconHome/></NavLink></div>
+                    <div className="item"><NavLink to="/workflow" activeClassName="active"><IconBars/></NavLink></div>
+                    <div className="item"><NavLink to="/inbox" activeClassName="active"><IconChart/></NavLink></div>
+                    <div className="item"><NavLink to="/projects" activeClassName="active"><IconMail/></NavLink></div>
+                    <div className="item"><NavLink to="/raport" activeClassName="active"><IconUser/></NavLink></div>
+                </div>
             </div>
         );
     }
