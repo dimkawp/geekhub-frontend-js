1f29fcc392d5f19bea95e485c0f3a55c30547d6f
dimkawp <dimkawp@gmail.com>
2018-04-14 16:27:57 +0300

    homework-14.0
    complite work on Inbox page
    work chat
    work message
    work create new croupChat

--- tasks/homework-14.0/homework/src/components/Main/Inbox/Inbox.js
+++ tasks/homework-14.0/homework/src/components/Main/Inbox/Inbox.js
@@ -6,7 +6,9 @@ import SideMenu from "../Menu/SideMenu";
 import Moment from 'react-moment';
 import 'moment-timezone';
 //bootstrap
-import { Row, Col } from 'reactstrap';
+import { Row, Col, } from 'reactstrap';
+import { Form, TextArea, Button } from 'semantic-ui-react';
+
 
 class Inbox extends Component {
     constructor(props) {
@@ -33,50 +35,104 @@ class Inbox extends Component {
                 {
                     id: 1,
                     userName: 'Michelle Stewart',
-                    data: '1976-04-19T12:59-0500',
-                    youMessage: [
-                        {id: 1, message: 'asd',data: ''},
-                        {id: 2, message: 'asd',data: ''},
-                        {id: 3, message: 'asd',data: ''}
-                    ],
-                    yourMessage: [
-                        {id: 1, message: 'asd',data: ''},
-                        {id: 2, message: 'asd',data: ''},
-                        {id: 3, message: 'asd',data: ''}
+                    data: '2018-04-13T12:59-0500',
+                    correspond: [
+                        {id: 1, message: 'More Random text again youMessage 1', init: 'youMessage', data: '2018-04-13T12:59-0500'},
+                        {id: 2, message: 'More Random text again youMessage 2', init: 'yourMessage', data: '2018-04-12T12:59-0500'},
+                        {id: 3, message: 'More Random text again youMessage 3', init: 'youMessage', data: '2018-04-11T12:59-0500'},
+                        {id: 4, message: 'More Random text again yourMessage 1',init: 'yourMessage', data: '2018-04-10T12:59-0500'},
+                        {id: 5, message: 'More Random text again yourMessage 2',init: 'youMessage', data: '2018-04-09T12:59-0500'},
+                        {id: 6, message: 'More Random text again yourMessage 3',init: 'yourMessage', data: '2018-04-08T12:59-0500'}
                     ],
                 },
                 {
                     id: 2,
                     userName: 'Michelle Stewart 2',
-                    data: '1976-04-19T12:59-0500',
-                    youMessage: [
-                        {id: 1, message: 'asd',data: ''},
-                        {id: 2, message: 'asd',data: ''},
-                        {id: 3, message: 'asd',data: ''}
-                    ],
-                    yourMessage: [
-                        {id: 1, message: 'asd',data: ''},
-                        {id: 2, message: 'asd',data: ''},
-                        {id: 3, message: 'asd',data: ''}
+                    data: '2018-04-13T12:59-0500',
+                    correspond: [
+                        {id: 1, message: 'More Random text again youMessage 1', init: 'yourMessage', data: '2018-04-13T12:59-0500'},
+                        {id: 2, message: 'More Random text again youMessage 2', init: 'youMessage', data: '2018-04-12T12:59-0500'},
+                        {id: 3, message: 'More Random text again youMessage 3', init: 'youMessage', data: '2018-04-11T12:59-0500'},
+                        {id: 4, message: 'More Random text again yourMessage 1',init: 'youMessage', data: '2018-04-10T12:59-0500'},
+                        {id: 5, message: 'More Random text again yourMessage 2',init: 'youMessage', data: '2018-04-09T12:59-0500'},
+                        {id: 6, message: 'More Random text again yourMessage 3',init: 'yourMessage', data: '2018-04-08T12:59-0500'}
                     ],
                 }
             ],
-            Correspond: {
-                youMessage: [
-                    {id: 1, message: 'asd',data: ''},
-                    {id: 2, message: 'asd',data: ''},
-                    {id: 3, message: 'asd',data: ''}
-                ],
-                yourMessage: [
-                    {id: 1, message: 'asd',data: ''},
-                    {id: 2, message: 'asd',data: ''},
-                    {id: 3, message: 'asd',data: ''}
-                ],
-            }
+            Correspond: [
+
+            ],
+            NewChatGroupButton: true,
+            TextMessage: '',
         };
+    }
+    CloseChatUserChangeGroup = (item) => {
+        this.setState({
+            Correspond: []
+        });
+    }
+    ChatUserChangeGroup = (item) => {
+        this.setState({
+            Correspond: item.correspond
+        });
+    }
+    handleChangeTextMessage = (e) => {
+        this.setState({TextMessage: e.target.value});
+        console.log(this.state.TextMessage);
+    }
+    createMessage = (e) => {
+        let newID = this.state.Correspond.length+1;
+        let message = this.state.TextMessage;
+        let newMessage =
+            {
+                id: newID,
+                message: message,
+                init: 'youMessage',
+                data: new Date()
+            }
+        ;
+
+        this.state.Correspond.push(newMessage);
+        if (message === 'hello') {
+            setTimeout(function () {
+                let answerMessage = 'hello i am bot Valera';
+                let newAnswerMessage = {
+                    id: this.state.Correspond.length+1,
+                    message: answerMessage,
+                    init: 'yourMessage',
+                    data: new Date()
+                }
+                this.state.Correspond.push(newAnswerMessage);
+                this.setState(this.state);
+                this.state;
+            }.bind(this), 3000);
+        }
+        this.state.TextMessage = '';
+        this.setState(this.state);
+        this.state;
+    }
+    NewCoversation = () => {
+        let newID = this.state.ChatUserGroup.length+1;
+        let NewCoversName = 'Michelle Stewart 3';
+        let NewDate = new Date();
+        let Corespond = [
+            {id: 1, message: this.state.User.userName+' Created new covers', init: 'youMessage', data: NewDate},
+        ];
+        let newCoversationGroup = {
+            id: newID,
+            userName: NewCoversName,
+            data: NewDate,
+            correspond: Corespond
+        }
+        this.state.ChatUserGroup.push(newCoversationGroup);
+        this.setState(this.state);
+        this.state;
+        this.state.NewChatGroupButton = !this.state.NewChatGroupButton;
+
     }
     render() {
         console.log("InboxComponents");
+        const User = this.state.User;
         return (
 
             <div className="wrapper">
@@ -103,73 +159,108 @@ class Inbox extends Component {
                             <Col xl="3">
                                 <div className="chatMessageGroup">
                                     <div className="items">
-                                        <div className="item">
-                                            <div className="header">
-                                                <div className="avatar">
-                                                    <img src="" alt=""/>
-                                                </div>
-                                                <div className="data"></div>
-                                            </div>
-                                            <div className="lastMessage">
-                                                <p></p>
-                                            </div>
-                                        </div>
+                                        {
+                                            this.state.ChatUserGroup.map((item) => {
+                                                return (
+                                                    <div className="item" key={item.id} onClick={(e)=>this.ChatUserChangeGroup(item)}>
+                                                        <div className="header">
+                                                            <div className="avatar">
+                                                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAFEkYKhA8fT4D4Q00B4_yVglFAOkMkUnncMaXaO1SDs8GhyOI" alt=""/>
+                                                                {item.userName}
+                                                            </div>
+                                                            <div className="date"><Moment format="YYYY-MM-DD HH:mm">{item.data}</Moment></div>
+                                                        </div>
+                                                        <div className="message">
+                                                            {
+                                                                item.correspond > 0 &&
+                                                                <p>{item.correspond.slice(-1)[0].message}</p>
+                                                            }
+                                                        </div>
+                                                    </div>
+                                                );
+                                            })
+                                        }
                                     </div>
+                                    {
+                                        this.state.NewChatGroupButton === true &&
+                                        <div className="newCovers"><Button onClick={this.NewCoversation}>New coversation</Button></div>
+                                    }
+
                                 </div>
                             </Col>
                             <Col xl="6">
                                 <div className="correspond">
-                                    <div className="yourMessage">
-                                        <div className="messageBody">
-                                            <p></p>
-                                            <div className="data"></div>
-                                        </div>
-                                        <div className="avatar">
-                                            <img src="" alt=""/>
-                                        </div>
-                                    </div>
-                                    <div className="youMessage">
-                                        <div className="avatat">
-                                            <img src="" alt=""/>
-                                        </div>
-                                        <div className="messageBody">
-                                            <p></p>
-                                            <div className="data"></div>
-                                        </div>
-                                    </div>
+                                    {
+                                        this.state.Correspond === [] &&
+                                            <div>...</div>
+                                    }
+                                    {
+                                        this.state.Correspond > [] &&
+                                        <Button onClick={this.CloseChatUserChangeGroup}>x</Button>
+                                    }
+
+                                    {
+                                        this.state.Correspond.map((item) => {
+                                            {
+                                                return (
+                                                    <div className={item.init} key={item.id}>
+                                                        <div className="messageBody">
+                                                            <p>{item.message}</p>
+                                                            <div className="date"><Moment format="YYYY-MM-DD HH:mm">{item.data}</Moment></div>
+                                                        </div>
+                                                        <div className="avatar">
+                                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAFEkYKhA8fT4D4Q00B4_yVglFAOkMkUnncMaXaO1SDs8GhyOI" alt=""/>
+                                                        </div>
+                                                    </div>
+                                                );
+                                            }
+                                        })
+                                    }
                                 </div>
                                 <div className="writeMessageBlock">
-                                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                                    {
+                                        this.state.Correspond === [] &&
+                                            <div>...</div>
+                                    }
+                                    {
+                                        this.state.Correspond > [] &&
+                                        <Form>
+                                            <TextArea rows={1} className="writeMessageTextarea" autoHeight placeholder='Write a message' value={this.state.TextMessage} onChange={this.handleChangeTextMessage}/>
+                                            <Button type='submit' onClick={this.createMessage}>WRITE</Button>
+                                        </Form>
+                                    }
+
                                 </div>
 
                             </Col>
                             <Col xl="3">
                                 <div className="userProfileInfo">
                                     <div className="avatar">
-                                        <img src="" alt=""/>
+                                        <div className="status"><div className="dot"></div></div>
+                                        <img src={User.avatarUrl} alt=""/>
                                     </div>
                                     <div className="title">
-                                        <div className="name"></div>
-                                        <span></span>
+                                        <div className="name">{User.userName}</div>
+                                        <span>{User.role}</span>
                                     </div>
                                     <div className="description">
-                                        <p></p>
+                                        <p>{User.description}</p>
                                     </div>
                                     <div className="email">
-                                        <span className="lable"></span>
-                                        <span></span>
+                                        <span className="lable">email</span>
+                                        <span>{User.email}</span>
                                     </div>
                                     <div className="phone">
-                                        <span className="lable"></span>
-                                        <span></span>
+                                        <span className="lable">phone</span>
+                                        <span>{User.phone}</span>
                                     </div>
                                     <div className="adress">
-                                        <span className="lable"></span>
-                                        <span></span>
+                                        <span className="lable">adress</span>
+                                        <span>{User.adress}</span>
                                     </div>
                                     <div className="Organization">
-                                        <span className="lable"></span>
-                                        <span></span>
+                                        <span className="lable">organization</span>
+                                        <span>{User.organization}</span>
                                     </div>
                                 </div>
                             </Col>
